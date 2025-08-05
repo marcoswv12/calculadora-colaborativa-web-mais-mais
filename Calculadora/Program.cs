4cc3f8f02774b29c60f47a260cdad50aee663ae7
marcoswv12 <marcos.wv122@gmail.com>
2025-08-05 16:34:17 -0300

    Update Program.cs

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,1 +1,28 @@
+using System;
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Calculadora");
+    }
+  static float soma()
+  {
+  }
+    static float subtração()
+  {
+  }
+    static float divisão()
+  {
+  }
+    static float multiplicação()
+  {
+  }
+    static float porcentagem()
+  {
+  }
+    static float potencia()
+  {
+  }
+  
+}
