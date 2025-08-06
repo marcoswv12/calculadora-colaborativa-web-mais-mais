1c04d5dde4f6983c6670e397b796d802ccddad4d
isabelleliimaa <lima.isabellet@gmail.com>
2025-08-05 23:08:49 -0300

    Update Program.cs
    Fazendo a função divisão.

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -32,6 +32,49 @@ class Program
   }
     static float divisão()
   {
+        bool continuar = true;
+        while (continuar)
+        {
+            Console.Clear();
+            Console.WriteLine("Calculadora de Divisão");
+            
+            try
+            {
+                Console.Write("Digite o primeiro numero:");
+                float num1 = float.Parse(Console.ReadLine());
+                
+                Console.Write("Digite o segundo numero:");
+                float num2 = float.Parse(Console.ReadLine());
+
+                if (num2 == 0)
+                {
+                    Console.WriteLine("Erro: a divisão por zero não é permitida.");
+                }
+                else
+                {
+                    float resultado = Dividir(num1, num2);
+                    Console.WriteLine($"Resultado: {num1} / {num2} = {resultado}");
+                }
+            }
+            catch (FormatException erro)
+            {
+                Console.WriteLine("Erro: Você digitou um valor invalido. Use apenas numeros.");
+            }
+            
+            Console.Write("\nDeseja fazer outra divisão? (s/n):");
+            string resposta = Console.ReadLine().ToLower();
+
+            if (resposta != "s")
+            {
+                continuar = false;
+            }
+        }
+        Console.WriteLine("Encerrando a calculadora. Até logo!");
+    }
+    static float Dividir(float x, float y)
+                         {
+                             return x/y;
+                         }
   }
     static float multiplicação()
   {
